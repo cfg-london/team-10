f88b1b8b14e264b818bd085f6fd8fbe4a9e1ffa9
WhoSketchy <dannywho@live.co.uk>
2017-11-18 00:30:34 +0000

    The initial database ahaha good luck danny

--- application/views/Login.php
+++ application/views/Login.php
@@ -5,6 +5,20 @@ defined ('BASEPATH') OR exit('No direct script access allowed');
 <html>
 <head>
 <title>Login</title>
+<script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA1iRB-ICOXZOf1tcj_YxuuD-fSypp5rzw",
+    authDomain: "cfg-team10.firebaseapp.com",
+    databaseURL: "https://cfg-team10.firebaseio.com",
+    projectId: "cfg-team10",
+    storageBucket: "",
+    messagingSenderId: "547270850669"
+  };
+  firebase.initializeApp(config);
+</script>
+
 <link rel = "stylesheet" type = "text/css"
 	href = "<?php echo base_url(); ?>css/login.css">
 </head>
